c46f63c20c66dfafad68ac58e4e3f63c4b94753d
Milind <milind.thakur@nzme.co.nz>
2016-08-02 15:35:06 +1200

    Refactor: Remove driver methods, rename quitting driver mehtod

--- core/browser/browser.py
+++ core/browser/browser.py
@@ -3,9 +3,6 @@ class Browser(object):
     def __init__(self, baseurl):
         self.baseUrl = baseurl
 
-    def create_webdriver(self):
-        raise NotImplementedError
-
     def get_browser_type(self):
         raise NotImplementedError
 
@@ -21,11 +18,8 @@ class Browser(object):
     def set_base_url(self, baseUrl):
         self.baseUrl = baseUrl
 
-    def get_webdriver(self):
-        raise NotImplementedError
-
     def get_default_desiredcapabilities(self):
         raise NotImplementedError
 
-    def quit_webdriver(self):
+    def quit(self):
         raise NotImplementedError
