a52254988d053cdb5b4dc905b1d9c99ff4ef4ee1
Milind <milind.thakur@nzme.co.nz>
2016-08-02 15:38:53 +1200

    Refactor unittest for driver init

--- test/test_driver_init.py
+++ test/test_driver_init.py
@@ -4,14 +4,15 @@ import unittest
 from core.browser.localbrowserbuilder import LocalBrowserBuilder
 
 
-class test_driver_init(unittest.TestCase):
+class DriverInitTestCase(unittest.TestCase):
     def test_local_chrome_init(self):
-        lb = LocalBrowserBuilder("chrome", "https://www.google.co.nz")
+        lb = LocalBrowserBuilder("phantomJS", "https://www.google.co.nz")
         self.browser = lb.build()
         assert self.browser.get_base_url() == "https://www.google.co.nz"
         assert self.browser.get_current_url() == "https://www.google.co.nz/"
-        self.browser.goto_url("http://www.bing.com/?cc=nz")
+        self.browser.goto_absolute_url("http://www.bing.com/?cc=nz")
+        assert self.browser.get_base_url() == "http://www.bing.com/?cc=nz"
         assert self.browser.get_current_url() == "http://www.bing.com/?cc=nz"
 
     def tearDown(self):
-        self.browser.quit_webdriver()
+        self.browser.quit()
