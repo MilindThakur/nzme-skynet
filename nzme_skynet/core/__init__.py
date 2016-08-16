7c805e65e1497111bb7416bfe0e009a0f3597984
Milind <milind.thakur@nzme.co.nz>
2016-08-16 15:49:45 +1200

    bump up the version; add init file at root level

--- nzme_skynet/core/__init__.py
+++ nzme_skynet/core/__init__.py
@@ -1,8 +1,9 @@
 # coding=utf-8
+"""Skynet Test Automation Package"""
 
-__author__      =   'Milind Thakur'
-__license__     =   ''
-__version__     =   '0.1.0'
-__maintainer__  =   __author__
-__email__       =   'milind.thakur@nzme.co.nz'
-__url__         =   'https://bitbucket.org/grabone/Skynet.git'
+__author__      = 'Milind Thakur'
+__license__     = ''
+__version__     = '0.1.1'
+__maintainer__  = __author__
+__email__       = 'milind.thakur@nzme.co.nz'
+__url__         = 'https://bitbucket.org/grabone/Skynet.git'
