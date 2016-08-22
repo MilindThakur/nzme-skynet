9bbfc6d1559455984b56af7f57edb2252a00156c
Milind <milind.thakur@nzme.co.nz>
2016-08-22 14:02:01 +1200

    Manage list of devices internally

--- test/test_layout.py
+++ test/test_layout.py
@@ -16,15 +16,13 @@ class LayoutTestCase(unittest.TestCase):
             shutil.rmtree(self.SCREENSHOT_PATH)
 
     def test_screenshot_with_default_download_folder(self):
-        sc = LayoutScreenshot(os.path.join(self.CUR_DIR,"testdata/urls.json"),
-                              os.path.join(self.CUR_DIR,"testdata/devices.json"))
+        sc = LayoutScreenshot(os.path.join(self.CUR_DIR,"testdata/urls.json"), "iPhone_4,iPhone_5")
         sc.take_screenshot()
         self.assertEqual(os.path.exists(self.SCREENSHOT_PATH), True, "Failed to create screenshot directory")
         self.assertEqual(len(os.listdir(self.SCREENSHOT_PATH)) > 0, True, "Failed to create snapshots")
 
     def test_screenshot_with_custom_download_folder(self):
-        sc = LayoutScreenshot(os.path.join(self.CUR_DIR, "testdata/urls.json"),
-                              os.path.join(self.CUR_DIR, "testdata/devices.json"),
+        sc = LayoutScreenshot(os.path.join(self.CUR_DIR, "testdata/urls.json"), "iPhone_6,iPad_4",
                               self.CUSTOM_DOWNLOAD_FOLDER)
         sc.take_screenshot()
         self.assertEqual(os.path.exists(self.CUSTOM_DOWNLOAD_FOLDER), True, "Failed to create customer screenshot directory")
