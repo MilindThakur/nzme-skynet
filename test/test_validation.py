8b3065a7707de94b10d6099d6f292cd3d78cb069
Donna McIntosh <donna.mcintosh@nzme.co.nz>
2016-08-25 09:44:11 +1200

    more debugging for folder path issue

--- test/test_validation.py
+++ test/test_validation.py
@@ -10,67 +10,58 @@ class ValidationTestCase(unittest.TestCase):
     DEFAULT_RESULTS_FOLDER_PATH = os.path.abspath('.') + "/%s" % "PageValidationResults"
     CUSTOM_RESULTS_FOLDER_PATH = os.path.abspath('.') + "/%s" % "ValidationResults"
 
-    def setUp(self):
-        if os.path.exists(self.CUSTOM_RESULTS_FOLDER_PATH):
-            shutil.rmtree(self.CUSTOM_RESULTS_FOLDER_PATH)
-        if os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH):
-            shutil.rmtree(self.DEFAULT_RESULTS_FOLDER_PATH)
 
-    def test_validate_no_urls_file(self):
-        im = Validation(os.path.join(self.CUR_DIR, "testdata/nourl.json"))
-        im.validate()
-        self.assertEqual(not os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH), True,
-                         "Not expecting results folder path to be created")
+    # def test_validate_no_urls_file(self):
+    #     im = Validation(os.path.join(self.CUR_DIR, "testdata/nourl.json"))
+    #     im.validate()
+    #     self.assertTrue(not os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH),
+    #                     "Not expecting results folder path to be created")
 
-    def test_validate_good_images_file(self):
-        im = Validation(os.path.join(self.CUR_DIR, "testdata/goodimages.json"))
-        im.validate()
-        self.assertEqual(not os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH), True,
-                         "Not expecting results folder path to be created")
+    # def test_validate_good_images_file(self):
+    #     im = Validation(os.path.join(self.CUR_DIR, "testdata/goodimages.json"))
+    #     im.validate()
+    #     self.assertFalse(os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH),
+    #                     "Not expecting results folder path to be created")
 
-    def test_validate_good_links_file(self):
-        im = Validation(os.path.join(self.CUR_DIR, "testdata/goodlinks.json"))
-        im.validate()
-        self.assertEqual(not os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH), True,
-                         "Not expecting results folder path to be created")
+    # def test_validate_good_links_file(self):
+    #     im = Validation(os.path.join(self.CUR_DIR, "testdata/goodlinks.json"))
+    #     im.validate()
+    #     self.assertFalse(os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH),
+    #                      "Not expecting results folder path to be created")
+    #
+    # def test_validate_broken_images_file(self):
+    #     im = Validation(os.path.join(self.CUR_DIR,"testdata/brokenimages.json"))
+    #     im.validate()
+    #     self.assertTrue(os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH), "Failed to create default results folder path")
+    #     self.assertTrue(len(os.listdir(self.DEFAULT_RESULTS_FOLDER_PATH)) == 1, "Failed to create result files")
+    #     self.assertTrue(os.stat(self.DEFAULT_RESULTS_FOLDER_PATH + "/" +
+    #                      os.listdir(self.DEFAULT_RESULTS_FOLDER_PATH)[0]).st_size != 0,
+    #                      "Expecting an invalid result for the test url")
 
-    def test_validate_broken_images_file(self):
-        im = Validation(os.path.join(self.CUR_DIR,"testdata/brokenimages.json"))
-        im.validate()
-        self.assertEqual(os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH), True, "Failed to create default results folder path")
-        self.assertEqual(len(os.listdir(self.DEFAULT_RESULTS_FOLDER_PATH)) == 1, True, "Failed to create result files")
-        self.assertEqual(os.stat(self.DEFAULT_RESULTS_FOLDER_PATH + "/" +
-                         os.listdir(self.DEFAULT_RESULTS_FOLDER_PATH)[0]).st_size != 0,
-                         True, "Expecting an invalid result for the test url")
-
-    def test_validate_broken_links_file(self):
-        im = Validation(os.path.join(self.CUR_DIR, "testdata/brokenlinks.json"))
-        im.validate()
-        self.assertEqual(os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH), True,
-                         "Failed to create default results folder path")
-        self.assertEqual(len(os.listdir(self.DEFAULT_RESULTS_FOLDER_PATH)) == 1, True,
-                         "Failed to create result files")
-        self.assertEqual(os.stat(self.DEFAULT_RESULTS_FOLDER_PATH + "/" +
-                                 os.listdir(self.DEFAULT_RESULTS_FOLDER_PATH)[0]).st_size != 0,
-                         True, "Expecting an invalid result for the test url")
+    # def test_validate_broken_links_file(self):
+    #     im = Validation(os.path.join(self.CUR_DIR, "testdata/brokenlinks.json"))
+    #     im.validate()
+    #     self.assertTrue(os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH), "Failed to create default results folder path")
+    #     self.assertTrue(len(os.listdir(self.DEFAULT_RESULTS_FOLDER_PATH)) == 1, "Failed to create result files")
+    #     self.assertTrue(os.stat(self.DEFAULT_RESULTS_FOLDER_PATH + "/" +
+    #                      os.listdir(self.DEFAULT_RESULTS_FOLDER_PATH)[0]).st_size != 0,
+    #                      "Expecting an invalid result for the test url")
 
     def test_validate_customresultsfolder(self):
         im = Validation(os.path.join(self.CUR_DIR, "testdata/brokenimages.json"), custom_results_path=self.CUSTOM_RESULTS_FOLDER_PATH)
         im.validate()
-        self.assertEqual(os.path.exists(self.CUSTOM_RESULTS_FOLDER_PATH), True,
-                         "Failed to create default results folder path")
-        self.assertEqual(len(os.listdir(self.CUSTOM_RESULTS_FOLDER_PATH)) == 1, True,
-                         "Failed to create result files")
-        self.assertEqual(os.stat(self.CUSTOM_RESULTS_FOLDER_PATH + "/" +
-                                 os.listdir(self.CUSTOM_RESULTS_FOLDER_PATH)[0]).st_size != 0,
-                         True, "Expecting an invalid result for the test url")
+        self.assertTrue(os.path.exists(self.CUSTOM_RESULTS_FOLDER_PATH), "Failed to create default results folder path")
+        self.assertTrue(len(os.listdir(self.CUSTOM_RESULTS_FOLDER_PATH)) == 1, "Failed to create result files")
+        self.assertTrue(os.stat(self.CUSTOM_RESULTS_FOLDER_PATH + "/" +
+                                os.listdir(self.CUSTOM_RESULTS_FOLDER_PATH)[0]).st_size != 0,
+                        "Expecting an invalid result for the test url")
 
 
-    # def tearDown(self):
-    #     if os.path.exists(self.CUSTOM_RESULTS_FOLDER_PATH):
-    #         shutil.rmtree(self.CUSTOM_RESULTS_FOLDER_PATH)
-    #     if os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH):
-    #         shutil.rmtree(self.DEFAULT_RESULTS_FOLDER_PATH)
+    def tearDown(self):
+        if os.path.exists(self.CUSTOM_RESULTS_FOLDER_PATH):
+            shutil.rmtree(self.CUSTOM_RESULTS_FOLDER_PATH)
+        if os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH):
+            shutil.rmtree(self.DEFAULT_RESULTS_FOLDER_PATH)
 
 if __name__ == "__main__":
     unittest.main()
