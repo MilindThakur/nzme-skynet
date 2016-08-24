4c1dd45960cd808bc132b652a78bd3c6d191096e
Donna McIntosh <donna.mcintosh@nzme.co.nz>
2016-08-25 09:15:35 +1200

    work in progress (john)

--- test/test_validation.py
+++ test/test_validation.py
@@ -6,17 +6,33 @@ from nzme_skynet.core.layout.validation import Validation
 
 
 class ValidationTestCase(unittest.TestCase):
-
     CUR_DIR = os.path.dirname(__file__)
-    DEFAULT_RESULTS_FOLDER_PATH = "./PageValidationResults"
-    #DEFAULT_RESULTS_FOLDER_PATH = os.path.abspath('.') + "/PageValidationResults"
-    CUSTOM_RESULTS_FOLDER_PATH = "./ValidationResults"
+    DEFAULT_RESULTS_FOLDER_PATH = os.path.abspath('.') + "/%s" % "PageValidationResults"
+    CUSTOM_RESULTS_FOLDER_PATH = os.path.abspath('.') + "/%s" % "ValidationResults"
 
     def setUp(self):
         if os.path.exists(self.CUSTOM_RESULTS_FOLDER_PATH):
             shutil.rmtree(self.CUSTOM_RESULTS_FOLDER_PATH)
         if os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH):
             shutil.rmtree(self.DEFAULT_RESULTS_FOLDER_PATH)
+
+    def test_validate_no_urls_file(self):
+        im = Validation(os.path.join(self.CUR_DIR, "testdata/nourl.json"))
+        im.validate()
+        self.assertEqual(not os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH), True,
+                         "Not expecting results folder path to be created")
+
+    def test_validate_good_images_file(self):
+        im = Validation(os.path.join(self.CUR_DIR, "testdata/goodimages.json"))
+        im.validate()
+        self.assertEqual(not os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH), True,
+                         "Not expecting results folder path to be created")
+
+    def test_validate_good_links_file(self):
+        im = Validation(os.path.join(self.CUR_DIR, "testdata/goodlinks.json"))
+        im.validate()
+        self.assertEqual(not os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH), True,
+                         "Not expecting results folder path to be created")
 
     def test_validate_broken_images_file(self):
         im = Validation(os.path.join(self.CUR_DIR,"testdata/brokenimages.json"))
@@ -38,18 +54,6 @@ class ValidationTestCase(unittest.TestCase):
                                  os.listdir(self.DEFAULT_RESULTS_FOLDER_PATH)[0]).st_size != 0,
                          True, "Expecting an invalid result for the test url")
 
-    def test_validate_good_images_file(self):
-        im = Validation(os.path.join(self.CUR_DIR, "testdata/goodimages.json"))
-        im.validate()
-        self.assertEqual(not os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH), True,
-                         "Not expecting results folder path to be created")
-
-    def test_validate_good_links_file(self):
-        im = Validation(os.path.join(self.CUR_DIR, "testdata/goodlinks.json"))
-        im.validate()
-        self.assertEqual(not os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH), True,
-                         "Not expecting results folder path to be created")
-
     def test_validate_customresultsfolder(self):
         im = Validation(os.path.join(self.CUR_DIR, "testdata/brokenimages.json"), custom_results_path=self.CUSTOM_RESULTS_FOLDER_PATH)
         im.validate()
@@ -61,17 +65,12 @@ class ValidationTestCase(unittest.TestCase):
                                  os.listdir(self.CUSTOM_RESULTS_FOLDER_PATH)[0]).st_size != 0,
                          True, "Expecting an invalid result for the test url")
 
-    def test_validate_no_urls_file(self):
-        im = Validation(os.path.join(self.CUR_DIR, "testdata/nourl.json"))
-        im.validate()
-        self.assertEqual(not os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH), True,
-                         "Not expecting results folder path to be created")
 
-    def tearDown(self):
-        if os.path.exists(self.CUSTOM_RESULTS_FOLDER_PATH):
-            shutil.rmtree(self.CUSTOM_RESULTS_FOLDER_PATH)
-        if os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH):
-            shutil.rmtree(self.DEFAULT_RESULTS_FOLDER_PATH)
+    # def tearDown(self):
+    #     if os.path.exists(self.CUSTOM_RESULTS_FOLDER_PATH):
+    #         shutil.rmtree(self.CUSTOM_RESULTS_FOLDER_PATH)
+    #     if os.path.exists(self.DEFAULT_RESULTS_FOLDER_PATH):
+    #         shutil.rmtree(self.DEFAULT_RESULTS_FOLDER_PATH)
 
 if __name__ == "__main__":
     unittest.main()
