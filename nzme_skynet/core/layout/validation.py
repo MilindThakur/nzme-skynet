96b83b778b401848bb74e28425157bb237ea8731
Donna McIntosh <donna.mcintosh@nzme.co.nz>
2016-08-26 13:39:02 +1200

    modified image, link and js methods to be public and updated unit tests accordingly.

--- nzme_skynet/core/layout/validation.py
+++ nzme_skynet/core/layout/validation.py
@@ -5,14 +5,10 @@ import requests
 
 from datetime import datetime
 from nzme_skynet.core.browsers.localbrowserbuilder import LocalBrowserBuilder
-from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-
-
-# noinspection PyMethodMayBeStatic,PyMethodMayBeStatic
 class Validation(object):
-    _DEFAULT_PATH = os.path.abspath('.') + "/ValidationResults"
+    _DEFAULT_PATH = os.path.abspath('./..') + "/ValidationResults"
     _DEFAULT_FILENAME = "%s_%s.txt" % ("pagevalidation_results", datetime.now().strftime("%Y%m%d-%H%M%S"))
 
 
@@ -21,8 +17,7 @@ class Validation(object):
             self.urls_json = json.load(urlf)
         capabilities = DesiredCapabilities.PHANTOMJS
         capabilities['loggingPrefs'] = {'browser': 'ALL'}
-        driver = webdriver.PhantomJS(desired_capabilities=capabilities)
-        lb = LocalBrowserBuilder("phantomJS")
+        lb = LocalBrowserBuilder("phantomJS", desCap=capabilities)
         browser = lb.build()
         self.mydriver = browser.driver
         if custom_results_path:
@@ -30,16 +25,16 @@ class Validation(object):
         else:
             self._results_path = self._DEFAULT_PATH
 
-    def validate(self):
+    def validateall(self):
         invalid_result = {}
         invalid_links = []
         invalid_images = []
         invalid_js = []
         for url in self.urls_json["urls"]:
             self.mydriver.get(url["url"])
-            invalid_images = self._validate_images_on_url()
-            invalid_links = self._validate_links_on_url()
-            invalid_js = self._validate_javascript_on_url()
+            invalid_images = self.validate_images_on_url()
+            invalid_links = self.validate_links_on_url()
+            invalid_js = self.validate_javascript_on_url()
         if invalid_links + invalid_images + invalid_js:
             invalid_result[url["url"]] = invalid_links + invalid_images + invalid_js
             self._create_folder_and_write_result_to_file(self._results_path, invalid_result)
@@ -48,31 +43,39 @@ class Validation(object):
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
-    def _validate_images_on_url(self):
-        images = self.mydriver.find_elements_by_tag_name("img")
-        broken_images_list = []
-        for image in images:
-            b = self.mydriver.execute_script("return arguments[0].complete && "
-                                             "typeof arguments[0].naturalWidth != \"undefined\" && "
-                                             "arguments[0].naturalWidth > 0", image)
-            if not b:
-                broken_images_list.append(image.get_attribute("src"))
+    def validate_images_on_url(self):
+        for url in self.urls_json["urls"]:
+            self.mydriver.get(url["url"])
+            images = self.mydriver.find_elements_by_tag_name("img")
+            broken_images_list = []
+            for image in images:
+                b = self.mydriver.execute_script("return arguments[0].complete && "
+                                                 "typeof arguments[0].naturalWidth != \"undefined\" && "
+                                                 "arguments[0].naturalWidth > 0", image)
+                if not b:
+                    broken_images_list.append(image.get_attribute("src"))
         return broken_images_list
 
-    def _validate_links_on_url(self):
-        links = self.mydriver.find_elements_by_xpath("//a[@href]")
-        broken_links_list = []
-        for link in links:
-            if link.is_displayed() and ("http" in link.get_attribute("href")):
-                if not self._is_link_broken(link.get_attribute("href")):
-                    broken_links_list.append(link.text)
+    def validate_links_on_url(self):
+        for url in self.urls_json["urls"]:
+            self.mydriver.get(url["url"])
+            links = self.mydriver.find_elements_by_xpath("//a[@href]")
+            broken_links_list = []
+            for link in links:
+                if link.is_displayed() and ("http" in link.get_attribute("href")):
+                    if not self._is_link_broken(link.get_attribute("href")):
+                        broken_links_list.append(link.text)
         return broken_links_list
 
-    def _validate_javascript_on_url(self):
+    def validate_javascript_on_url(self):
         broken_js_list = []
-        for entry in self.mydriver.get_log('browser'):
-            if entry:
-                broken_js_list.append(str(entry['level'] + ": " + entry['message']))
+        log_level = ['WARNING', 'SEVERE', 'FATAL', 'ERROR']
+        for url in self.urls_json["urls"]:
+            self.mydriver.get(url["url"])
+            errors = self.mydriver.get_log('browser')
+            for entry in errors:
+                if entry['level'] in log_level:
+                    broken_js_list.append(str(entry['level'] + ": " + entry['message']))
         return broken_js_list
 
     def _is_link_broken(self, link):
@@ -88,6 +91,7 @@ class Validation(object):
             for n in result[i]:
                 target.write(' %s'%(repr(str(n))))
                 target.write('\n')
+            target.write('\n' * 2)
         target.close()
 
 
