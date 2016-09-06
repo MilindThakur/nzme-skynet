3633f2bf5e6afccd434ca9889a7d58781213c16d
Milind <milind.thakur@nzme.co.nz>
2016-09-07 11:49:16 +1200

    more CI debug

--- nzme_skynet/core/layout/urlvalidation.py
+++ nzme_skynet/core/layout/urlvalidation.py
@@ -42,8 +42,8 @@ def _validate_links_on_url(url, driver, open_url=True):
     if validate_url(url, driver):
         links = driver.find_elements_by_xpath("//a[@href]")
         for link in links:
-            print link.get_attribute("href")
             if link.is_displayed() and ("http" in link.get_attribute("href")):
+                print requests.get(link.get_attribute("href")).status_code
                 if not (requests.get(link.get_attribute("href")).status_code == 200):
                     broken_links_list.append(link.text)
         return broken_links_list
