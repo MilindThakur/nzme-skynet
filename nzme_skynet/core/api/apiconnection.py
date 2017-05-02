919c4eadad29f4bca08a2d72c3d2b2d658052fc7
Milind <milind.thakur@nzme.co.nz>
2017-05-02 13:23:18 +1200

    Make API wrapper more generic

--- nzme_skynet/core/api/apiconnection.py
+++ nzme_skynet/core/api/apiconnection.py
@@ -21,31 +21,21 @@ class ApiConnection(object):
         else:
             return "https://" + self.host
 
-    def _build_header(self, content_type):
-        header = {"Accept": "application/json"}
-        if content_type != "":
-            header["Content-type"] = content_type
-        return header
+    def get(self, uri, params=None, headers=None, auth=None):
+        return self._request(uri, "GET", params=params, headers=headers, auth=auth)
 
-    def get(self, uri, params=None, content_type="application/json"):
-        if params is None:
-            params = {}
-        return self._request(uri, "GET", params=params, content_type=content_type)
+    def post(self, uri, params=None, json=None, data=None, headers=None):
+        return self._request(uri, "POST", params=params, json=json, data=data, headers=headers)
 
-    def post(self, uri, json=None, data=None, content_type="application/json"):
-        return self._request(uri, "POST", json=json, data=data, content_type=content_type)
+    def put(self, uri, json, headers=None):
+        return self._request(uri, "PUT", json=json, headers=headers)
 
-    def put(self, uri, json, content_type="application/json"):
-        return self._request(uri, "PUT", json=json, content_type=content_type)
-
-    def patch(self, uri, json, content_type="application/json"):
-        return self._request(uri, "PATCH", json=json, content_type=content_type)
+    def patch(self, uri, json, headers=None):
+        return self._request(uri, "PATCH", json=json, headers=headers)
 
     def delete(self, uri):
         return self._request(uri, "DELETE")
 
-    def _request(self, uri, method, params=None, json=None, data=None, content_type=None):
+    def _request(self, uri, method, params=None, json=None, data=None, headers=None, auth=None):
         h = self._create_connection_url()
-        r = requests.request(method=method, url=h + uri, params=params, json=json, data=data,
-                             headers=self._build_header(content_type))
-        return r
+        return requests.request(method=method, url=h + uri, params=params, json=json, data=data, headers=headers, auth=auth)
