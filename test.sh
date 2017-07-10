39d9979a07f9c22099c79600c8ab68c8d77fda15
Milind <milind.thakur@nzme.co.nz>
2017-07-10 15:36:31 +1200

    Add integration test for docker grid setup

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+./docker_compose.sh start
 cd test/testserver
 python -m SimpleHTTPServer &>/dev/null &
 cd ../../
 sleep 2
 py.test test
 pkill -f SimpleHTTPServer
+./docker_compose.sh stop
