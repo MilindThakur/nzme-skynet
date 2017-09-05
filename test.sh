75fa10a601cddef5c7fb25ce7a6203c7e611562f
milind.thakur <milind.thakur@grabone.co.nz>
2017-09-05 13:35:58 +1200

    Increase sleep for emulators to start in CI

--- test.sh
+++ test.sh
@@ -4,7 +4,8 @@ cd test/testserver
 python -m SimpleHTTPServer &>/dev/null &
 cd ../../
 # Wait for the emulators to start
-sleep 20
+echo "Sleeping for 1m for emulators to start..."
+sleep 1m
 py.test -v test
 pkill -f SimpleHTTPServer
 ./docker_compose.sh stop
