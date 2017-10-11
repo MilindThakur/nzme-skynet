446514532a85b2cba364df3124ae0047c2d1a84d
milind.thakur <milind.thakur@grabone.co.nz>
2017-10-12 09:53:43 +1300

    Update tests for driverfactory

--- test/test_driver_factory.py
+++ test/test_driver_factory.py
@@ -7,20 +7,29 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.phantomjs.webdriver import WebDriver
 from selenium.webdriver.remote.webdriver import WebDriver
 from nzme_skynet.core.driver.enums.drivertypes import DriverTypes
+from nzme_skynet.core.driver.web.browserdriver import BrowserDriver
 
 
 class DriverFactoryTest(unittest.TestCase):
+
+    def test_no_driver_registered_exception(self):
+        with self.assertRaises(Exception) as context:
+            DriverRegistry.get_driver()
+        self.assertTrue("No registered driver found" in context.exception.message)
+
+    def test_no_driver_deregistration_exception(self):
+        with self.assertRaises(Exception) as context:
+            DriverRegistry.deregister_driver()
+        self.assertTrue('No registered driver found' in context.exception)
 
     # Chrome browser does not run in CI without headless mode.
     # Test should fail on CI but pass locally.
     @unittest.expectedFailure
     def test_default_chrome_local_driver_creation(self):
-        with self.assertRaises(Exception) as context:
-            DriverRegistry.get_driver()
-        self.assertTrue("No registered driver found" in context.exception.message)
-        DriverRegistry.register_driver("chrome")
-        self.assertEqual(DriverRegistry.get_driver().name, DriverTypes.CHROME)
-        self.assertIsInstance(DriverRegistry.get_driver(), WebDriver)
+        DriverRegistry.register_driver()
+        self.assertEqual(DriverRegistry.get_webdriver().name, DriverTypes.CHROME)
+        self.assertIsInstance(DriverRegistry.get_driver(), BrowserDriver)
+        self.assertIsInstance(DriverRegistry.get_webdriver(), WebDriver)
         DriverRegistry.deregister_driver()
         with self.assertRaises(Exception) as context:
             DriverRegistry.get_driver()
@@ -28,8 +37,9 @@ class DriverFactoryTest(unittest.TestCase):
 
     def test_custom_local_driver_registration(self):
         DriverRegistry.register_driver(DriverTypes.PHANTOMJS)
-        self.assertEqual(DriverRegistry.get_driver().name, DriverTypes.PHANTOMJS)
-        self.assertIsInstance(DriverRegistry.get_driver(), WebDriver)
+        self.assertEqual(DriverRegistry.get_webdriver().name, DriverTypes.PHANTOMJS)
+        self.assertIsInstance(DriverRegistry.get_driver(), BrowserDriver)
+        self.assertIsInstance(DriverRegistry.get_webdriver(), WebDriver)
         DriverRegistry.deregister_driver()
         with self.assertRaises(Exception) as context:
             DriverRegistry.get_driver()
@@ -42,8 +52,9 @@ class DriverFactoryTest(unittest.TestCase):
 
     def test_default_remote_chrome_driver_registration(self):
         DriverRegistry.register_driver(local=False)
-        self.assertIsInstance(DriverRegistry.get_driver(), WebDriver)
-        self.assertEqual(DriverRegistry.get_driver().name, DriverTypes.CHROME)
+        self.assertIsInstance(DriverRegistry.get_driver(), BrowserDriver)
+        self.assertIsInstance(DriverRegistry.get_webdriver(), WebDriver)
+        self.assertEqual(DriverRegistry.get_webdriver().name, DriverTypes.CHROME)
         DriverRegistry.deregister_driver()
         with self.assertRaises(Exception) as context:
             DriverRegistry.get_driver()
@@ -51,8 +62,9 @@ class DriverFactoryTest(unittest.TestCase):
 
     def test_custom_remote_driver_registration(self):
         DriverRegistry.register_driver(driver_type=DriverTypes.FIREFOX, local=False)
-        self.assertIsInstance(DriverRegistry.get_driver(), WebDriver)
-        self.assertEqual(DriverRegistry.get_driver().name, DriverTypes.FIREFOX)
+        self.assertIsInstance(DriverRegistry.get_driver(), BrowserDriver)
+        self.assertIsInstance(DriverRegistry.get_webdriver(), WebDriver)
+        self.assertEqual(DriverRegistry.get_webdriver().name, DriverTypes.FIREFOX)
         DriverRegistry.deregister_driver()
         with self.assertRaises(Exception) as context:
             DriverRegistry.get_driver()
@@ -72,8 +84,9 @@ class DriverFactoryTest(unittest.TestCase):
             "javascriptEnabled": True
         }
         DriverRegistry.register_driver(driver_options=test_capabilities, local=False)
-        self.assertIsInstance(DriverRegistry.get_driver(), WebDriver)
-        self.assertEqual(DriverRegistry.get_driver().name, DriverTypes.CHROME)
+        self.assertIsInstance(DriverRegistry.get_driver(), BrowserDriver)
+        self.assertIsInstance(DriverRegistry.get_webdriver(), WebDriver)
+        self.assertEqual(DriverRegistry.get_webdriver().name, DriverTypes.CHROME)
         DriverRegistry.deregister_driver()
         with self.assertRaises(Exception) as context:
             DriverRegistry.get_driver()
