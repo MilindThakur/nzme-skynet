06d37f7579563cf064631ff56ee39a848d637e42
milind.thakur <milind.thakur@grabone.co.nz>
2017-10-13 11:19:57 +1300

    Set ios app driver capabilities

--- nzme_skynet/core/driver/mobile/app/iosappdriver.py
+++ nzme_skynet/core/driver/mobile/app/iosappdriver.py
@@ -13,6 +13,30 @@ class IOSAppDriver(MAppDriver):
     def _create_driver(self):
         self._driver = WebDriver(command_executor=self._remote_url, desired_capabilities=self._desired_cap)
 
+    def _set_default_capabilities(self):
+        self._create_desired_capabilities()
+        self._desired_cap['fullReset'] = 'True'
+
+    def _create_desired_capabilities(self):
+        if not self._desired_cap:
+            raise Exception("No capabilities provided to init Appium driver")
+        if not self._desired_cap['platformVersion']:
+            raise Exception("Please provide platformVersion to test app against")
+        if not self._desired_cap['bundleId']:
+            raise Exception("Please provide bundle ID")
+        if not self._desired_cap['app']:
+            raise Exception("Please provide absolute app path for .ipa")
+        if not self._desired_cap['appActivity']:
+            raise Exception("Please provide the activity to launch")
+
+        self._desired_cap['platformName'] = 'iOS'
+        self._desired_cap['platform'] = 'iOS'
+        if not self._desired_cap['deviceName']:
+            # Run tests on Android emulator by default
+            self._desired_cap['deviceName'] = 'iPhone Simulator'
+        if not self._desired_cap['automation']:
+            self._desired_cap['automation'] = 'XCUITest'
+
     @property
     def webdriver(self):
         # type: () -> WebDriver
