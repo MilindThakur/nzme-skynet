8c44e48ff0c9c73e9e7312fbda1b7fa73bc15309
milind.thakur <milind.thakur@grabone.co.nz>
2017-10-12 17:18:57 +1300

    Implement android and ios app driver

--- nzme_skynet/core/driver/mobile/app/androiddriver.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-from nzme_skynet.core.driver.mobile.app.mobileapp import MobileApp
-
-
-class AndroidDriver(MobileApp):
-
-    def __init__(self, desired_caps):
-        super(AndroidDriver, self).__init__(desired_caps)
---
+++ nzme_skynet/core/driver/mobile/app/iosappdriver.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+from nzme_skynet.core.driver.mobile.app.mappdriver import MAppDriver
+from appium.webdriver.webdriver import WebDriver
+
+
+class IOSAppDriver(MAppDriver):
+
+    def __init__(self, desired_capabilities, remote_url='http://127.0.0.1:4444/wd/hub'):
+        self._desired_cap = desired_capabilities
+        self._remote_url = remote_url
+        self._driver = None
+
+    def _create_driver(self):
+        self._driver = WebDriver(command_executor=self._remote_url, desired_capabilities=self._desired_cap)
+
+    @property
+    def webdriver(self):
+        # type: () -> WebDriver
+        return self._driver
