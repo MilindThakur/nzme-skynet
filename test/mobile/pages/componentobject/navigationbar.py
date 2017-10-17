415f01c43e9d7497073ba305f41c2f5c5e8e9b52
milind.thakur <milind.thakur@grabone.co.nz>
2017-10-17 14:39:16 +1300

    Delete the redundant feature tests

--- test/mobile/pages/componentobject/navigationbar.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class NavigationBar(object):
-    def __init__(self, app):
-        self.app = app
-        self.bar = self.app.get_actions().mobelement(By.ID,"navigation")
-        self.navigation_cell = self.app.get_actions().mobelement(By.ID, "navigation")
