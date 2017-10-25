74aafa419850de0108e23ec778754ae5fd3db8f1
milind.thakur <milind.thakur@grabone.co.nz>
2017-10-26 09:59:05 +1300

    Allow importing interactive elements through controls module

--- nzme_skynet/core/controls/__init__.py
+++ nzme_skynet/core/controls/__init__.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+
+from nzme_skynet.core.controls.button import Button
+from nzme_skynet.core.controls.checkbox import Checkbox
+from nzme_skynet.core.controls.element import Element
+from nzme_skynet.core.controls.image import Image
+from nzme_skynet.core.controls.melement import MElement
+from nzme_skynet.core.controls.radiobutton import RadioButton
+from nzme_skynet.core.controls.table import Table
+from nzme_skynet.core.controls.text import Text
+from nzme_skynet.core.controls.textinput import TextInput
+from nzme_skynet.core.controls.textlink import TextLink
+from nzme_skynet.core.controls.select import SelectElem
