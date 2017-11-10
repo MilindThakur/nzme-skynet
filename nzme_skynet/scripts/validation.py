52f8d4b06f3fc41440e11713219f8bb6809233ab
stefan kahn <Stefan.kahn@nzme.co.nz>
2017-11-10 16:13:26 +1300

    Added logging into the url validator.

--- nzme_skynet/scripts/validation.py
+++ nzme_skynet/scripts/validation.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 import argparse
+import logging
+import logging.config
 from datetime import datetime
 import json
 from collections import defaultdict
@@ -19,31 +21,31 @@ def main():
     args = parser.parse_args()
     results = defaultdict(dict)
     list_urls = args.urls.split(',')
+    if args.folder:
+        setup_logging(args.folder)
     for url in list_urls:
-        if not args.folder:
-            print 'URL : ' + url
         if args.checkall:
+            log('Checking all for URL : ' + url)
             args.checklinks = args.checkjs = args.checkimages = True
         if args.checklinks:
-            if args.folder:
-                results[url]['Link Errors:'] = str(validate_links(url))
-            else:
-                print 'Link Errors: ' + str(validate_links(url))
+            log('Checking Links for URL : ' + url)
+            results[url]['Link Errors:'] = str(validate_links(url))
         if args.checkimages:
-            if args.folder:
-                results[url]['Image Errors:'] = str(validate_images(url))
-            else:
-                print 'Image Errors: ' + str(validate_images(url))
+            log('Checking images for URL : ' + url)
+            results[url]['Image Errors:'] = str(validate_images(url))
         if args.checkjs:
-            if args.folder:
-                results[url]['JS Errors:'] = str(validate_js_error(url))
-            else:
-                print 'JS Errors: ' + str(validate_js_error(url))
-    if args.folder:
-        create_folder_and_write_result_to_file(args.folder, results)
+            log('Checking JS errors for URL : ' + url)
+            results[url]['JS Errors:'] = str(validate_js_error(url))
+    log("Results:")
+    log(json.dumps(results, sort_keys=True, indent=4))
 
 
-def create_folder_and_write_result_to_file(results_path, result):
-    filename = results_path + "/%s_%s.json" % ("val_results", datetime.now().strftime("%Y%m%d-%H%M%S"))
-    with open(filename, 'w') as output:
-        json.dump(result, output, sort_keys=True, indent=4)
+def setup_logging(filename):
+    logging.basicConfig(filename=filename+"/%s_%s" % ("val_results", datetime.now().strftime("%Y%m%d-%H%M%S")+".log")
+                        , level=logging.INFO)
+
+
+def log(log_entry):
+    print log_entry
+    if len(logging.root.handlers) > 1:
+        logging.info(log_entry)
