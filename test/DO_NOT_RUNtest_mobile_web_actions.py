e85feada249e6c45021cc5d41d4b5147bcaa56af
milind.thakur <milind.thakur@grabone.co.nz>
2018-05-21 09:44:39 +1200

    Fix the test for android

--- test/DO_NOT_RUNtest_mobile_web_actions.py
+++ test/DO_NOT_RUNtest_mobile_web_actions.py
@@ -1,12 +1,17 @@
 # coding=utf-8
 import unittest
+from selenium.webdriver.common.by import By
 
+from nzme_skynet.core.controls.textinput import TextInput
+from nzme_skynet.core.controls.button import Button
+from nzme_skynet.core.controls.element import Element
+from nzme_skynet.core.controls.text import Text
 from nzme_skynet.core.driver.driverregistry import DriverRegistry
 from nzme_skynet.core.driver.enums.drivertypes import DriverTypes
 from nzme_skynet.core.driver.mobile.browser.androidbrowserdriver import AndroidBrowserDriver
 
 TEST_URL = "https://www.google.co.nz"
-DOCKER_SELENIUM_URL = "http://localhost:4444/wd/hub"
+DOCKER_SELENIUM_URL = "http://localhost:4723/wd/hub"  # Appium server
 
 
 class MobileActionsTestCase(unittest.TestCase):
@@ -37,6 +42,15 @@ class MobileActionsTestCase(unittest.TestCase):
         self.driver.goto_url(TEST_URL, absolute=True)
         assert TEST_URL in self.driver.current_url
         assert 'Google' in self.driver.title
+        search_input = TextInput(By.NAME, 'q')
+        search_btn = Button(By.CSS_SELECTOR, 'button[aria-label="Google Search"]')
+        search_input.set_value("NZME")
+        search_btn.click()
+        results = Element(By.ID, 'ires')
+        assert results.will_be_visible(), "Error: Results not visible"
+        result_text = Text(By.CSS_SELECTOR, "#rso > div:nth-child(1) > div > div > div:nth-child(1) > div:nth-child(1)"
+                                            " > a > div")
+        assert result_text.has_text('NZME'), "Error: No NZME result found"
 
     @classmethod
     def tearDownClass(cls):
