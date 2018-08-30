274df5febc9ff65e3be9af839d1dfae1362191d5
milind.thakur <milind.thakur@grabone.co.nz>
2018-08-31 09:08:11 +1200

    Add chrome support method

--- nzme_skynet/core/driver/mobile/browser/mbrowserdriver.py
+++ nzme_skynet/core/driver/mobile/browser/mbrowserdriver.py
@@ -9,3 +9,9 @@ class MBrowserDriver(MobileDriver, BrowserDriver):
     def __init__(self, desired_capabilities, remote_url='http://127.0.0.1:4444/wd/hub'):
         super(MBrowserDriver, self).__init__(desired_capabilities, remote_url)
         self._browser = self._desired_cap['browserName']
+
+    def add_chrome_options(self, option):
+        if "chromeOptions" in self._desired_cap:
+            self._desired_cap['chromeOptions']['args'].append(option)
+        else:
+            self._desired_cap['chromeOptions'] = {'args': [option]}
